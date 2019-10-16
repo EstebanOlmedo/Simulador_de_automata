9d1077e41423413b4332ef4e4513317057d2fd36
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2019-10-15 20:35:51 -0500

    Se agrego la clase EvaluadorDeExpresion

---
+++ EvaluadorDeExpresion.java
@@ -0,0 +1,66 @@
+public class EvaluadorDeExpresion{
+	private MaquinaDeTuring maquina;
+	private AutomataAPila automataPila;
+	private AutomataFinitoNoDeterminista automataFinitoDeterminista;
+	private AutomataFinitoNoDeterminista automataFinitoNoDeterminista;
+	private AutomataFinitoNoDeterministaEpsilon automataFinitoNoDeterministaEpsilon;
+
+	public EvaluadorDeExpresion(
+		MaquinaDeTuring maquina,
+		AutomataAPila automataPila,
+		AutomataFinitoDeterminista automataFinitoDeterminista,
+		AutomataFinitoNoDeterminista automataFinitoNoDeterminista,
+		AutomataFinitoNoDeterministaEpsilon automataFinitoNoDeterministaEpsilon)
+	{
+		this.maquina = maquina;
+		this.automataPila = automataPila;
+		this.automataFinitoDeterminista = automataFinitoDeterminista;
+		this.automataFinitoNoDeterminista = automataFinitoNoDeterminista;
+		this.automataFinitoNoDeterministaEpsilon = automataFinitoNoDeterministaEpsilon;
+	}
+
+	public EvaluadorDeExpresion(){
+		this(null, null, null, null, null);
+	}
+
+	public EvaluadorDeExpresion(EvaluadorDeExpresion evaluador){
+		this(evaluador.maquina,
+			evaluador.automataPila,
+			evaluador.automataFinitoDeterminista,
+			evaluador.automataFinitoNoDeterminista,
+			evaluador.automataFinitoNoDeterministaEpsilon);
+	}
+
+	public void destruir(){
+		if(maquina != null) maquina = null;
+		if(automataPila != null) automataPila = null;
+		if(automataFinitoDeterminista != null) automataFinitoDeterminista = null;
+		if(automataFinitoNoDeterminista != null) automataFinitoNoDeterminista = null;
+		if(automataFinitoNoDeterministaEpsilon != null) automataFinitoNoDeterministaEpsilon = null;
+		System.gc(); 
+	}
+
+	@Override
+	public String toString(){
+		String cad = "";
+		cad += "Maquina de turing: "+maquina+"\n";
+		cad += "Automata a pila: "+automataPila+"\n";
+		cad += "Automata Finito Determinista: "+automataFinitoDeterminista+"\n";
+		cad += "Automata Finito No Determinista: "+automataFinitoNoDeterminista+"\n";
+		cad += "Automata Finito No Determinista Epsilon: "+automataFinitoNoDeterministaEpsilon+"\n";
+		return cad;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if(obj == null) return false;
+		if(!(obj instanceof EvaluadorDeExpresion)) return false;
+		EvaluadorDeExpresion evaluador = (EvaluadorDeExpresion)obj;
+		return maquina.equals(evaluador.maquina) &&
+		automataPila.equals(evaluador.automataPila) &&
+		automataFinitoDeterminista.equals(evaluador.automataFinitoDeterminista) &&
+		automataFinitoNoDeterminista.equals(evaluador.automataFinitoNoDeterminista) &&
+		automataFinitoNoDeterministaEpsilon.equals(evaluador.automataFinitoNoDeterministaEpsilon);
+	}
+
+}
