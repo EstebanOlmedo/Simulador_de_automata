e75d29a2da486992a5ed46958da6b567bfca2633
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2019-10-17 02:39:57 -0500

    Se agregó el metodo getInforme() a GeneradorDeMaquinaDeTuring

--- GeneradorDeMaquinaDeTuring.java
+++ GeneradorDeMaquinaDeTuring.java
@@ -1,34 +1,48 @@
-public class GeneradorDeMaquinaDeTuring{
+public class GeneradorDeMaquinaDeTuring
+{
 	private MaquinaDeTuring maquina;
 
-	public GeneradorDeMaquinaDeTuring(MaquinaDeTuring maquina){
+	public GeneradorDeMaquinaDeTuring(MaquinaDeTuring maquina)
+	{
 		this.maquina = maquina;
 	}
 
-	public GeneradorDeMaquinaDeTuring(){
-		this(null);
+	public GeneradorDeMaquinaDeTuring()
+	{
+		this(new MaquinaDeTuring());
 	}
 
-	public GeneradorDeMaquinaDeTuring(GeneradorDeMaquinaDeTuring generador){
+	public GeneradorDeMaquinaDeTuring(GeneradorDeMaquinaDeTuring generador)
+	{
 		this(generador.maquina);
 	}
 
-	public void destruir(){
+	public void destruir()
+	{
 		if(maquina != null) maquina = null;
 		System.gc();
 	}
 
 	@Override
-	public String toString(){
+	public String toString()
+	{
 		return "Maquina de Turing: " + maquina;
 	}
 
 	@Override
-	public boolean equals(Object obj){
+	public boolean equals(Object obj)
+	{
 		if(obj == null) return false;
 		if(!(obj instanceof GeneradorDeMaquinaDeTuring)) return false;
 		GeneradorDeMaquinaDeTuring generador = (GeneradorDeMaquinaDeTuring)obj;
 		return maquina.equals(generador.maquina);
 	}
-
+	public String getInforme()
+	{
+		return "Maquina de turing generada\nInformacion de la maquina:\n";
+	}
+	public String getInforme(MaquinaDeTuring maquina)
+	{
+		return getInforme() + maquina;
+	}
 }
