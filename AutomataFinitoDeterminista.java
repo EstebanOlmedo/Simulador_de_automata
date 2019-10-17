3843185d1f9e0acefe18771a6e6a38a8f6342dfc
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2019-10-16 23:11:03 -0500

    Se cambió el tipo de retorno del método obtenerTransicion de la clase AutomataFinitoDeterminista

--- AutomataFinitoDeterminista.java
+++ AutomataFinitoDeterminista.java
@@ -26,33 +26,12 @@ public class AutomataFinitoDeterminista extends AutomataFinito
 			tablaDeTransiciones = null;
 		System.gc();
 	}
-	public String obtenerTransicion(int q)
-	{
-		String retorno = "";
-		ArrayList<Integer> estado = tablaDeTransiciones.get(q);
-		for (int i=0; i<estado.size(); i++)
-			retorno += "S("+q+","+i+") -> "+estado.get(i)+"\n";
-		return retorno;
-	}
-	public String obtenerTransicion(int q, int p)
-	{
-		String retorno = "";
-		ArrayList<Integer> estado = tablaDeTransiciones.get(q);
-		for (int i=0; i<estado.size(); i++)
-			if(estado.get(i) == p)
-				retorno += "S("+q+","+i+") -> "+estado.get(i)+"\n";
-		return retorno;
-	}
 	@Override 
 	public String toString()
 	{
 		String retorno = new String();
 		retorno += super.toString();
 		int i = 0;
-		/*
-		tablaDeTransiciones.forEach((estado) ->	
-				retorno += "Estado: " + (++i) + estado);
-		*/
 		for(ArrayList <Integer> adyacenciaEstado: tablaDeTransiciones)
 		{
 			retorno += "Estado " + (++i) + ": " + adyacenciaEstado + "\n";
@@ -67,5 +46,24 @@ public class AutomataFinitoDeterminista extends AutomataFinito
 		AutomataFinitoDeterminista automata = (AutomataFinitoDeterminista)obj;
 		return super.equals(automata) && 
 			tablaDeTransiciones.equals(automata.tablaDeTransiciones);
+	}
+	public ArrayList<Integer> obtenerTransicion(int q)
+	{
+		if(q > tablaDeTransiciones.size())
+			return null;
+		ArrayList<Integer> estado = tablaDeTransiciones.get(q);
+		return estado;
+	}
+	public ArrayList<Integer> obtenerTransicion(int q, int p)
+	{
+		ArrayList<Integer> estado = obtenerTransicion(q);
+		ArrayList<Integer> transiciones = new ArrayList<Integer>();
+		if(estado == null){return null;}
+		for (int i=0; i<estado.size(); i++)
+		{
+			if(estado.get(i) == p)
+				transiciones.add(i);
+		}
+		return transiciones;
 	}
 }
