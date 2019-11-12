e18c90a6aaf3e45f04b55cc6eb98d003d7d1333f
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2019-11-11 21:28:51 -0600

    Se implemetó el método crearAutomataFinito()

--- GeneradorDeAutomataFinito.java
+++ GeneradorDeAutomataFinito.java
@@ -1,13 +1,16 @@
 /**
  * @author Daniel Montes Guerrero
  */
+import java.util.TreeMap;
 public class GeneradorDeAutomataFinito
 {
 	private AutomataFinito automataFinito;
+	private Teclado teclado;
 
 	public GeneradorDeAutomataFinito(AutomataFinito automataFinito)
 	{
 		this.automataFinito = automataFinito;
+		teclado = new Teclado();
 	}
 	public GeneradorDeAutomataFinito(){
 		this(new AutomataFinito());
@@ -42,6 +45,23 @@ public class GeneradorDeAutomataFinito
 		return automataFinito.equals(generador.automataFinito);
 	}
 	public AutomataFinito crearAutomataFinito(){
-		return new AutomataFinito();
+		System.out.println("Creando Automata...");
+		int numeroDeEstados = teclado.dameUnInt("Ingresa la cardinalidad del conjunto de estados");
+		int numeroDeEstadosAceptacion = teclado.dameUnInt("Ingresa la cardinalidad del conjunto de estados de aceptacion");
+		int[] aceptacion = new int[numeroDeEstadosAceptacion];
+		for(int i=0; i<numeroDeEstadosAceptacion; i++){
+			aceptacion[i] = teclado.dameUnInt("Ingresa el "+i+"-esimo estado de aceptacion");
+		}
+		int cardinalidad = teclado.dameUnInt("Ingresa la cardinalidad del alfabeto");
+		char[] alfabeto = new char[cardinalidad];
+		for(int i=0; i<cardinalidad; i++){
+			alfabeto[i] = teclado.dameUnChar("Ingresa el simbolo "+(i+1)+" del alfabeto");
+		}
+		AutomataFinito automata = new AutomataFinito(numeroDeEstados,alfabeto,aceptacion,new TreeMap<Character,Integer>());
+		automata.setMapa();
+		return automata;
+	}
+	public Teclado getTeclado(){
+		return teclado;
 	}
 }
