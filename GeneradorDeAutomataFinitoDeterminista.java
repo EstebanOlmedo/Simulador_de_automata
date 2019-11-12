af8c1eb0e18da808f31849ce004d95ffab9d4064
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2019-11-11 21:56:07 -0600

    Se realizaron correcciones al método crearAutomataFinitoDeterminista()

--- GeneradorDeAutomataFinitoDeterminista.java
+++ GeneradorDeAutomataFinitoDeterminista.java
@@ -53,11 +53,12 @@ public class GeneradorDeAutomataFinitoDeterminista extends GeneradorDeAutomataFi
 		for(int i=0; i<automata.getNumeroDeEstados(); i++){
 			ArrayList<Integer> estado = new ArrayList<Integer>();
 			for(int j=0; j<automata.getMapa().size(); j++){
-				int estadoDestino = super.getTeclado().dameUnInt("Ingresa el valor de S(q"+i+","+automata.getSimbolo(j)+")");
+				int estadoDestino = getTeclado().dameUnInt("Ingresa el valor de S(q"+i+","+automata.getSimbolo(j)+")");
 				estado.add(estadoDestino);
 			}
 			tabla.add(estado);
 		}
+		System.out.println("El automata fue creado con exito :)");
 		return new AutomataFinitoDeterminista(
 			automata.getNumeroDeEstados(),
 			automata.getAlfabeto(),
