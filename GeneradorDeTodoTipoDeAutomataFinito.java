62e44a858e0daf000f5ce006d524cca6fcdccbb0
brielgah <gabrielgh@live.com.mx>
2019-11-14 09:12:50 -0600

    correciones en el constructor

--- GeneradorDeTodoTipoDeAutomataFinito.java
+++ GeneradorDeTodoTipoDeAutomataFinito.java
@@ -27,10 +27,10 @@ public class GeneradorDeTodoTipoDeAutomataFinito
 	public GeneradorDeTodoTipoDeAutomataFinito()
 	{
 		this(
-				new AutomataFinitoDeterminista(), 
-				new AutomataFinitoNoDeterministaEpsilon(),
-			       	new AutomataFinitoNoDeterministaEpsilon(),
-			       	new AutomataFinitoAPila()
+				null, 
+				null,
+			       	null,
+			       	null
 				);
 	}
 
