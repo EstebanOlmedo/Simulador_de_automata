026a2d0365fd61b512338dcdfed4eed6e1807569
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2019-11-17 13:48:44 -0600

    Se agregó la excepción NoExisteEnAlfabetoException

---
+++ NoExisteEnAlfabetoException.java
@@ -0,0 +1,25 @@
+public class NoExisteEnAlfabetoException extends Exception
+{
+	private String mensaje;
+
+	public NoExisteEnAlfabetoException()
+	{
+		super("La cadena no pertenece al alfabeto");
+		mensaje = "La cadena no pertenece al alfabeto";
+	}
+	public NoExisteEnAlfabetoException(char simbolo)
+	{
+		super("El simbolo: " + simbolo + "no existe en el alfabeto");
+		mensaje = "El simbolo: " + simbolo + "no existe en el alfabeto";
+	}
+	public NoExisteEnAlfabetoException(String mensaje)
+	{
+		super(mensaje);
+		this.mensaje = mensaje;
+	}
+	@Override
+	public String toString()
+	{
+		return mensaje;
+	}
+}
