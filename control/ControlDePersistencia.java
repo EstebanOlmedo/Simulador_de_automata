8afbec3d8c4b5b0b44251213f407ce750733dcc3
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2019-11-24 18:25:58 -0600

    Se añadieron las clases ControlGeneradorDeMaquinaDeTuring ControlDePersistencia

---
+++ control/ControlDePersistencia.java
@@ -0,0 +1,81 @@
+package control;
+
+import persistencia.GeneradorYLectorDeArchivo;
+import vista.Teclado;
+/**
+ *
+ * @author Esteban Olmedo
+ */
+public class ControlDePersistencia 
+{
+	private GeneradorYLectorDeArchivo generadorYLector;
+	private Teclado teclado;
+	public ControlDePersistencia(Teclado teclado)
+        {
+		this.teclado = teclado;
+	}
+	public ControlDePersistencia(Teclado teclado, 
+                GeneradorYLectorDeArchivo generadorYLector)
+	{
+                this.teclado = teclado;
+                this.generadorYLector = generadorYLector;
+	}
+        public ControlDePersistencia(ControlDePersistencia control)
+	{
+		this(control.teclado, control.generadorYLector);
+	}
+        public ControlDePersistencia()
+        {
+		this.generadorYLector = new GeneradorYLectorDeArchivo();
+		this.teclado = new Teclado();
+	}
+	public void destruir()
+	{
+		if(generadorYLector != null){
+			generadorYLector.destruir();
+			generadorYLector = null;
+		}
+		if(teclado != null){
+			teclado.destruir();
+			teclado = null;
+		}
+		System.gc();
+	}
+	@Override
+	public String toString()
+	{
+		return "Control de persistencia\n";
+	}
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj == null){return false;}
+		if(!(obj instanceof ControlDePersistencia))
+			return false;
+		ControlDePersistencia control = (ControlDePersistencia)obj;
+		return teclado.equals(control.teclado) &&
+			generadorYLector.equals(control.generadorYLector);
+	}
+	
+	public void guardarUnObjeto(Object obj)
+	{
+		String nombre = teclado.dameUnString("Ingresa el nombre "
+			+ "del archivo en el que deseas guardar");
+		String path = teclado.dameUnString("Ingresa la ruta del archivo");
+		if(generadorYLector.escribirObjetoArchivo(obj, nombre, path))
+		{
+			System.out.println("Se ha guardado correctamente");
+		}
+		else
+		{
+			System.out.println("Hubo un error");
+		}
+	}
+	public Object recuperarObjeto()
+	{
+		String nombre = teclado.dameUnString("Ingresa el nombre "
+			+ "del archivo en el que deseas guardar");
+		String path = teclado.dameUnString("Ingresa la ruta del archivo");
+		return generadorYLector.leerObjetoArchivo(nombre, path);
+	}
+}
---
+++ control/ControlGeneradorDeMaquinaDeTuring.java
@@ -0,0 +1,107 @@
+/**
+ * @author Esteban Olmedo
+ */
+package control;
+
+import java.util.ArrayList;
+import logica.NoExisteEstadoException;
+import vista.Teclado;
+import logica.GeneradorDeMaquinaDeTuring;
+
+public class ControlGeneradorDeMaquinaDeTuring
+{
+	private Teclado teclado;
+	private GeneradorDeMaquinaDeTuring generador;
+
+	public ControlGeneradorDeMaquinaDeTuring(Teclado teclado)
+	{
+		this.teclado = teclado;
+		generador = new GeneradorDeMaquinaDeTuring();
+	}
+	public ControlGeneradorDeMaquinaDeTuring(ControlGeneradorDeMaquinaDeTuring control)
+	{
+		this(control.teclado, control.generador);
+	}
+	public ControlGeneradorDeMaquinaDeTuring(
+			Teclado teclado, GeneradorDeMaquinaDeTuring generador)
+	{
+		this.generador = generador;
+		this.teclado = teclado;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Control del generador de maquina de Turing";
+	}
+	@Override
+	public boolean equals(Object obj)
+	{
+		if(obj == null){return false;}
+		if(!(obj instanceof ControlGeneradorDeMaquinaDeTuring))
+			return false;
+		ControlGeneradorDeMaquinaDeTuring control = (ControlGeneradorDeMaquinaDeTuring)obj;
+		return teclado.equals(control.teclado) &&
+			generador.equals(control.generador);
+	}
+	
+	public void generarMaquinaDeTuring()
+	{	
+		String descripcion = teclado.dameUnString("¿Cuál es la descripción de la máquina? (El lenguaje que representa)");
+		int tamanioAlfabeto = teclado.dameUnInt("¿Cuál es la cardinalidad del alfabeto? (Excluyendo el símbolo 'B' (blanco)");
+		char[] alfabeto = new char[tamanioAlfabeto + 1];
+		System.out.println("A continuación se ingresarán los carácteres del alfabeto");
+		for(int i = 0; i < tamanioAlfabeto; i++)
+			alfabeto[i] = teclado.dameUnChar("Ingresa el caracter del alfabeto:");
+		alfabeto[tamanioAlfabeto] = 'B';
+		int numeroDeEstados = teclado.dameUnInt("¿Cuál es el número de estados de la Máquina de Turing");
+		System.out.println("A continuación ingrese los estados de aceptación de la máquina (indexados en cero)");
+		ArrayList<Integer> estadosAceptacion = new ArrayList<>();
+		while(true)
+		{
+			try
+			{
+				int estado = teclado.dameUnInt("Ingresa el número del estado de aceptación o -1 cando hayas terminado");
+				if(estado > numeroDeEstados - 1) throw new NoExisteEstadoException(estado);
+				if(estado == -1)
+					break;
+				estadosAceptacion.add(estado);
+			}
+			catch(NoExisteEstadoException neee)
+			{
+				System.out.println(neee);
+			}
+		}
+		generador.crearEstructuraMaquina(new String(alfabeto), numeroDeEstados, estadosAceptacion, descripcion);
+		generarEstadosMaquinaDeTuring(numeroDeEstados, tamanioAlfabeto, alfabeto);
+	}
+	private void generarEstadosMaquinaDeTuring(int numeroDeEstados, int tamanioAlfabeto,
+			char[] alfabeto)
+	{
+		for(int i = 0; i < numeroDeEstados; i++)
+		{
+			for(int j = 0; j < tamanioAlfabeto + 1; j++)
+			{
+				System.out.println("Para el estado q" + i + " con el símbolo " +
+						alfabeto[j] + ":");
+				try
+				{
+					int estadoDeTransicion = teclado.dameUnInt("¿A qué estado cambia? Ingresa -1 si no hay transición");
+					if(estadoDeTransicion > numeroDeEstados)
+						throw new NoExisteEstadoException();
+					if(estadoDeTransicion != -1)
+					{
+						char loQueDeja = teclado.dameUnChar("¿Qué es lo que deja en la cinta?");
+						char movimiento = teclado.dameUnChar("¿Hacia dónde se mueve el cabezal?");
+						generador.aniadirTransicionATabla(i, j, estadoDeTransicion, loQueDeja, movimiento);
+					}
+					else
+						generador.aniadirTransicionATabla(i, j, -1, '-', '-');
+				}catch(NoExisteEstadoException neee){
+					j--;
+					neee.printStackTrace();
+				}
+			}
+		}
+	}
+}
