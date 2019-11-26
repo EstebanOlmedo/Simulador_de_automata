b17ba0b15601a1d99d25c06a83834f4924110af8
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2019-11-26 02:14:24 -0600

    Para evaluar con Automatas se dejó una referencia a la clase padre

--- logica/EvaluadorDeExpresion.java
+++ logica/EvaluadorDeExpresion.java
@@ -4,35 +4,24 @@ package logica;
  */
 public class EvaluadorDeExpresion{
 	private MaquinaDeTuring maquina;
-	private AutomataFinitoAPila automataPila;
-	private AutomataFinitoDeterminista automataFinitoDeterminista;
-	private AutomataFinitoNoDeterminista automataFinitoNoDeterminista;
-	private AutomataFinitoNoDeterministaEpsilon automataFinitoNoDeterministaEpsilon;
+	private AutomataFinito automata;
 
 	public EvaluadorDeExpresion(
 		MaquinaDeTuring maquina,
-		AutomataFinitoAPila automataPila,
-		AutomataFinitoDeterminista automataFinitoDeterminista,
-		AutomataFinitoNoDeterminista automataFinitoNoDeterminista,
-		AutomataFinitoNoDeterministaEpsilon automataFinitoNoDeterministaEpsilon)
+		AutomataFinito automata)
 	{
 		this.maquina = maquina;
-		this.automataPila = automataPila;
-		this.automataFinitoDeterminista = automataFinitoDeterminista;
-		this.automataFinitoNoDeterminista = automataFinitoNoDeterminista;
-		this.automataFinitoNoDeterministaEpsilon = automataFinitoNoDeterministaEpsilon;
+		this.automata = automata;
 	}
 
 	public EvaluadorDeExpresion(){
-		this(null, null, null, null, null);
+		this(null, null);
 	}
 
 	public EvaluadorDeExpresion(EvaluadorDeExpresion evaluador){
 		this(evaluador.maquina,
-			evaluador.automataPila,
-			evaluador.automataFinitoDeterminista,
-			evaluador.automataFinitoNoDeterminista,
-			evaluador.automataFinitoNoDeterministaEpsilon);
+			evaluador.automata
+		);
 	}
 
 	public void destruir(){
@@ -40,21 +29,9 @@ public class EvaluadorDeExpresion{
 			maquina.destruir();
 			maquina = null;
 		}
-		if(automataFinitoDeterminista != null){
-			automataFinitoNoDeterminista.destruir();
-			automataFinitoDeterminista = null;
-		}
-		if(automataFinitoNoDeterminista != null){
-			automataFinitoNoDeterminista.destruir();
-			automataFinitoNoDeterminista = null;
-		}
-		if(automataFinitoNoDeterministaEpsilon != null){
-			automataFinitoNoDeterministaEpsilon.destruir();
-			automataFinitoNoDeterministaEpsilon = null;
-		}
-		if(automataPila != null){
-			automataPila.destruir();
-			automataPila = null;
+		if(automata != null){
+			automata.destruir();
+			automata = null;
 		}
 		System.gc(); 
 	}
@@ -63,10 +40,7 @@ public class EvaluadorDeExpresion{
 	public String toString(){
 		String cad = "";
 		cad += "Maquina de turing: "+maquina+"\n";
-		cad += "Automata a pila: "+automataPila+"\n";
-		cad += "Automata Finito Determinista: "+automataFinitoDeterminista+"\n";
-		cad += "Automata Finito No Determinista: "+automataFinitoNoDeterminista+"\n";
-		cad += "Automata Finito No Determinista Epsilon: "+automataFinitoNoDeterministaEpsilon+"\n";
+		cad += "Automata Finito: "+automata+"\n";
 		return cad;
 	}
 
@@ -76,10 +50,7 @@ public class EvaluadorDeExpresion{
 		if(!(obj instanceof EvaluadorDeExpresion)) return false;
 		EvaluadorDeExpresion evaluador = (EvaluadorDeExpresion)obj;
 		return maquina.equals(evaluador.maquina) &&
-		automataPila.equals(evaluador.automataPila) &&
-		automataFinitoDeterminista.equals(evaluador.automataFinitoDeterminista) &&
-		automataFinitoNoDeterminista.equals(evaluador.automataFinitoNoDeterminista) &&
-		automataFinitoNoDeterministaEpsilon.equals(evaluador.automataFinitoNoDeterministaEpsilon);
+		automata.equals(evaluador.automata);
 	}
 	public boolean perteneceAlAlfabeto(char caracter, AutomataFinito automata)
 	{
@@ -133,11 +104,4 @@ public class EvaluadorDeExpresion{
 		}
 		return false;
 	}
-
-	public void setAutomataFinitoDeterminista(AutomataFinitoDeterminista afd)
-	{
-		this.automataFinitoDeterminista = afd;
-	}
-
-
 }
